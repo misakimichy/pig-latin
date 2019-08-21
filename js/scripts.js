2b6a4f72862981913282584757b337aabf63854e
Makijane Gilbert and Misaki Koonce <misaki@>
2019-08-21 11:30:53 -0700

    Access to the first vowel of input[i]

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,19 @@ $(document).ready(function(){
     event.preventDefault();
     var input = $("input").val().split(' ');
     console.log(input);
-    for(var i = 0; i < input.length; i++){
-      for(var j = 0; j < vowels.length; j++){
+    for(var i = 0; i < input.length; i++) {
+      for(var j = 0; j < vowels.length; j++) {
         if(input[i].charAt(0) === vowels[j]) {
           console.log(input[i] + 'way');
+        } else {
+            for (let letter of input[i]){
+              if(letter === vowels[j]) {
+                // index number of first vowel in input[i]
+                var firstVowel = input[i].indexOf(letter);
+                }
+              }
+            };
+          }
         }
-      }
-    }
-  });
-});
+      });
+    });
