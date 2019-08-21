ab769648f54011f48d3bdda65ee60cfa8f64d6c5
Makijane Gilbert and Misaki Koonce <makijane@>
2019-08-21 16:46:16 -0700

    translate any sentence the user inputs into pig latin unless there is a y somewhere in the middle.

--- js/scripts.js
+++ js/scripts.js
@@ -5,33 +5,44 @@ $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var input = $("input").val().split('.').join('').split(' ');
-    console.log(input);
 
     for(var i = 0; i < input.length; i++) {
-        if(vowels.includes(input[i].charAt(0))) {
-          console.log(input[i] + 'way');
-        } else if (input[i].includes(special) === true){
-          for (let letter of input[i]) {
-            if(letter === special) {
-              // Find q index
-              var qIndex = input[i].indexOf(letter);
-              var qu = input[i].slice(qIndex, qIndex + 2);
-              var restOfQu = input[i].slice(qIndex + 2, input[i].length);
-              console.log(restOfQu + qu + 'ay');
-            }
+      if(vowels.includes(input[i].charAt(0))) {
+          $(".results").append("<span>" + input[i] + "way </span> ");
+      } else if (input[i].includes(special) === true){
+        for (let letter of input[i]) {
+          if(letter === special) {
+            // Find q index
+            var qIndex = input[i].indexOf(letter);
+            var qu = input[i].slice(qIndex, qIndex + 2);
+            var restOfQu = input[i].slice(qIndex + 2, input[i].length);
+            $(".results").append("<span>" + restOfQu + qu + "ay </span> ");
           }
-        } else {
-            for (let letter of input[i]){
-              if(vowels.includes(letter)) {
-                // index number of first vowel in input[i]
-                var firstVowelIndex = input[i].indexOf(letter);
-                var beforeVowel = input[i].slice(0, firstVowelIndex).toLowerCase();
-                var restLetters = input[i].slice(firstVowelIndex, input[i].length);
-                console.log(restLetters + beforeVowel + 'ay');
-                break;
-                }
-              }
-            };
         }
+      } else {
+          for (let letter of input[i]){
+            if(vowels.includes(letter)) {
+              // index number of first vowel in input[i]
+              var firstVowelIndex = input[i].indexOf(letter);
+              var beforeVowel = input[i].slice(0, firstVowelIndex).toLowerCase();
+              var restLetters = input[i].slice(firstVowelIndex, input[i].length);
+              $(".results").append("<span>" + restLetters + beforeVowel + "ay </span> ");
+
+            } else if (letter.includes("y")){
+              var indexOfY = input[i].indexOf("y");
+              var beforeY = input[i].slice(0, indexOfY);
+              var remainder = input[i].slice(indexOfY, input[i].length);
+              $(".results").append("<span>" + remainder + beforeY + "ay </span>")
+            }
+            }
+
+          }
+
+          $(".results").show();
+
+
+
+        };
       });
+
     });
